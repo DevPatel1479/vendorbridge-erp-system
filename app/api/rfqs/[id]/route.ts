e4dac8e00331c48d0032a2e1a0c6bc193bfef710
Dev Patel <devpatel1828king@gmail.com>
2026-06-06 14:01:58 +0530

    fixed the RFO endpoints

--- app/api/rfqs/[id]/route.ts
+++ app/api/rfqs/[id]/route.ts
@@ -1,13 +1,17 @@
 import { NextResponse } from "next/server";
 import { RFQService } from "@/services/rfq.service";
+import { getUserIdFromRequest } from "@/lib/auth";
 
 // GET BY ID
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    const rfq = await RFQService.getRFQById(params.id);
+    const { id } = await params;
+
+    const rfq = await RFQService.getRFQById(id);
+
     return NextResponse.json(rfq);
   } catch (err: any) {
     return NextResponse.json(
@@ -20,12 +24,13 @@ export async function GET(
 // UPDATE RFQ
 export async function PUT(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
+     const { id } = await params; 
     const body = await req.json();
 
-    const updated = await RFQService.updateRFQ(params.id, body);
+    const updated = await RFQService.updateRFQ(id, body);
 
     return NextResponse.json(updated);
   } catch (err: any) {
@@ -39,10 +44,21 @@ export async function PUT(
 // DELETE RFQ
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
-    await RFQService.deleteRFQ(params.id);
+    const userId = getUserIdFromRequest(req);
+
+    const { id } = await params; // ✅ FIX HERE
+
+    if (!id) {
+      return NextResponse.json(
+        { error: "Missing RFQ id" },
+        { status: 400 }
+      );
+    }
+
+    await RFQService.deleteRFQ(id, userId);
 
     return NextResponse.json({
       message: "RFQ deleted successfully",
--- lib/auth.ts
+++ lib/auth.ts
@@ -17,3 +17,16 @@ export function generateToken(payload: AuthUser) {
 export function verifyToken(token: string): AuthUser {
   return jwt.verify(token, JWT_SECRET) as AuthUser;
 }
+
+
+export function getUserIdFromRequest(req: Request) {
+  const authHeader = req.headers.get("authorization");
+
+  if (!authHeader) throw new Error("No token");
+
+  const token = authHeader.split(" ")[1];
+
+  const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
+
+  return decoded.id;
+}
